21ec2c08194798d815bc451cd72c066f02333958
Jake Worth <jake@jakeworth.com>
2016-12-09 13:54:31 -0600

    Remove unneeded spaces

--- lib/hello_world.rb
+++ lib/hello_world.rb
@@ -2,7 +2,7 @@ module HelloWorld
 
   # Greets the user by name, or by saying "Hello, World!" if no name is given.
   class << self
-    def hello(name = 'World')
+    def hello(name='World')
       "Hello, #{name}!"
     end
   end
